9f0f9a65f94c28c822faaddcccad8396e983e6e6
Jason Dunn <jason.dunn@vehicletracking.com>
2021-07-28 08:56:12 -0400

    adding connection to store

--- src/app/game/play/play.component.ts
+++ src/app/game/play/play.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-play',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) {
+    const things = db.collection('things').valueChanges();
+    things.subscribe(console.log);
+}
 
   ngOnInit(): void {
   }
