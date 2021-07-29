3e59dddcef0ab733f9d2f9bbce9947e0586079c5
Jason Dunn <jason.dunn@vehicletracking.com>
2021-07-29 11:23:33 -0400

    adding subscriber to game and initiating game

--- src/app/game/play/play.component.ts
+++ src/app/game/play/play.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireFunctions } from '@angular/fire/functions';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { Game } from 'src/app/models/game.model';
 
 @Component({
@@ -10,21 +11,29 @@ import { Game } from 'src/app/models/game.model';
   styleUrls: ['./play.component.css']
 })
 export class PlayComponent implements OnInit {
-  data$: Observable<any>;
+  // data$: Observable<any>;
   game$: Observable<any> = this.db.collection('game').doc('UeBXOQTPVdMz23LSUYTX').valueChanges();
   game: any;
+
+  destroy$: Subject<void> = new Subject<void>();
   constructor(private db: AngularFirestore, private fns: AngularFireFunctions) {
 
-    this.game$.subscribe(e => this.game = e);
+    this.game$.pipe(takeUntil(this.destroy$)).subscribe(e => this.game = e);
 
-    const callable = fns.httpsCallable('helloWorld');
-    this.data$ = callable({ name: 'some-data' });
+    // const callable = fns.httpsCallable('helloWorld');
+    // this.data$ = callable({ name: 'some-data' });
 }
 
   ngOnInit(): void {
 
-    this.data$.subscribe(e => console.log(e));
+    // this.data$.subscribe(e => console.log(e));
 
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
   }
 
 }
